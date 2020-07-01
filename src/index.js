46889ece70204b437569e94d4face1fd914c4d2e
Maksim Zhbanov <maksim.zhbanov@gmail.com>
2020-07-01 19:45:52 +0300

    fix: dependence

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import * as React from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
 
-const App = props => <React.Component>{props}</React.Component>
-
 const root = document.getElementById('root')
 
 if (root !== null) {
-  ReactDOM.render(<App />, root)
+  ReactDOM.render(<div></div>, root)
 }
--- webpack.docs.config.js
+++ webpack.docs.config.js
@@ -30,7 +30,6 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      favicon: './src/assets/favicon.ico',
     }),
   ],
   resolve: {
